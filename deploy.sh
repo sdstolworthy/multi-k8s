5ed6f37fe07f123793f5340a63c8f99cd9037451
Spencer Stolworthy <sdstolworthy@gmail.com>
2020-03-12 12:24:28 -0600

    updated travis

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,13 @@
-docker build -t mrcactus/multi-client -f ./client/Dockerfile ./client
-docker build -t mrcactus/multi-server -f ./server/Dockerfile ./server
-docker build -t mrcactus/multi-worker -f ./worker/Dockerfile ./worker
-docker push mrcactus/multi-server
-docker push mrcactus/multi-client
-docker push mrcactus/multi-worker
+docker build -t mrcactus/multi-client:latest -t mrcactus/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t mrcactus/multi-server:latest -t mrcactus/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t mrcactus/multi-worker:latest -t mrcactus/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push mrcactus/multi-server:latest
+docker push mrcactus/multi-server:$SHA
+docker push mrcactus/multi-client:latest
+docker push mrcactus/multi-client:$SHA
+docker push mrcactus/multi-worker:latest
+docker push mrcactus/multi-worker:$SHA
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=mrcactus/multi-server
+kubectl set image deployments/server-deployment server=mrcactus/multi-server:$SHA
+kubectl set image deployments/client-deployment client=mrcactus/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=mrcactus/multi-worker:$SHA
